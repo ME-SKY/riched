e658de9c331dd70730f01c93cdefaafa14af1593
ME-SKY <meskalitofly@gmail.com>
2019-09-28 08:22:11 +1000

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import * as rxJs from 'rxjs';
+import { debounce } from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'riched';
+
+  constructor(){}
+
+
+  mainframe = new rxJs.BehaviorSubject('');
+
+  distanceFromMaintainContainer = 30;
+  editorSize = {
+    width: 0,
+    height: 0
+  };
+
+  ngOnInit(){
+    console.log('initwidth and height')
+    console.dir(this.editorSize)
+  }
+
+  @HostListener('window:resize', ['$event.target'])
+  resizeWindow(target){
+    console.log('resize')
+    debounce(this.calculateEditorSize, 150)();
+  }
+
+  calculateEditorSize = () => {
+    console.log("calculateEditorSize")
+    this.editorSize.height = window.innerHeight - this.distanceFromMaintainContainer;
+    this.editorSize.width = window.innerWidth - this.distanceFromMaintainContainer;
+    console.log('editorSize')
+    console.dir(this.editorSize)
+  }
+
 }
